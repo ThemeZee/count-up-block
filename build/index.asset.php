724aa9c147a67bf1bb1af3c6d2650fbcf8e4a10c
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-06-27 16:18:42 +0200

    added support for colors and typography

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '7c6a2ff63a4cb1076528');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '427da619d825848340ab');
