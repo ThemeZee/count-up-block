db5bc53a2ac4ddcb7aef3ed99f6f06daf7944727
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-06-28 10:12:03 +0200

    added border settings and dimensions

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '0be1b703c6c782ef11e7');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '53475a03c500de3d7ae9');
